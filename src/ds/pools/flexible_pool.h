e6ff78a93f5c1c0c7047037370f61242f8e01873
Duy Nguyen <nhdtxdy@gmail.com>
2025-06-06 09:04:18 +0200

    [DS] Pool update: FlexiblePool (mine) and BS::ThreadPool (library code from bshoshany on GitHub)

---
+++ src/ds/pools/flexible_pool.h
@@ -0,0 +1,184 @@
+#ifndef FLEXIBLE_POOL_H
+#define FLEXIBLE_POOL_H
+
+#include <vector>
+#include <thread>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <cassert>
+
+enum class ControlSignal { OK, RESET, STOP };
+
+// FLEXIBLEPOOL IS NOT THREAD-SAFE! (ONLY ONE THREAD SUPPOSED TO HAVE ACCESS TO THE THREAD POOL) 
+template<template<class> class QueueType>
+class FlexiblePool {
+public:
+    using TaskType = std::function<ControlSignal()>;
+    
+    explicit FlexiblePool(unsigned int num_workers);
+    explicit FlexiblePool(unsigned int num_workers, QueueType<TaskType> &&tasks);
+    ~FlexiblePool();
+
+    void start();
+    
+    template <class F, class... Args>
+    void push(F&& f, Args&&... args);
+    void stop();
+    void reset();
+
+    int get_active_workers() const {
+        return num_active_workers;
+    }
+
+private:
+    unsigned int num_workers;
+    std::vector<std::thread> workers;
+    QueueType<TaskType> tasks;
+    std::thread::id owner;
+
+    bool stopped = false;
+    std::atomic<bool> running{false};
+
+    std::atomic<int> num_active_workers{0};
+
+    void do_work();
+
+};
+
+template<template<class> class QueueType>
+void FlexiblePool<QueueType>::start() {
+    running = true;
+    running.notify_all();
+}
+
+template<template<class> class QueueType>
+void FlexiblePool<QueueType>::reset() {
+    if (std::this_thread::get_id() != owner) {
+        std::cerr << "[FlexiblePool] [ERROR] Non-owning thread attempted to reset the thread pool.\n";
+        return;
+    }
+
+    // Need to ensure there are active workers first
+    int cur_active_workers;
+    while ((cur_active_workers = num_active_workers) == 0) {
+        num_active_workers.wait(cur_active_workers);
+    }
+
+    running = false;
+    running.notify_all();
+    
+    stopped = false;
+
+    for (unsigned int i = 0; i < num_workers; ++i) {
+        tasks.push([]() -> ControlSignal {
+            return ControlSignal::RESET;
+        });
+    }
+    
+    // Wait for all workers to acknowledge reset
+    while ((cur_active_workers = num_active_workers) > 0) {
+        num_active_workers.wait(cur_active_workers);
+    }
+}
+
+template<template<class> class QueueType>
+void FlexiblePool<QueueType>::do_work() {
+    // Signal that this worker has reset and is now waiting
+    while (true) {
+        running.wait(false);
+        num_active_workers.fetch_add(1);
+        num_active_workers.notify_all();
+
+        while (true) {
+            TaskType task;
+            if (tasks.pop(task)) {
+                ControlSignal signal = task();
+                if (signal == ControlSignal::STOP) {
+                    num_active_workers.fetch_sub(1);
+                    num_active_workers.notify_all();
+                    return;
+                }
+                else if (signal == ControlSignal::RESET) {
+                    num_active_workers.fetch_sub(1);
+                    num_active_workers.notify_all();
+                    break;
+                }
+            }
+            else if (!tasks.is_blocking()) { // known at compile-time as is_blocking is constexpr function
+                std::this_thread::yield();
+            }
+            else {
+                std::cerr << "[FlexiblePool] [FATAL] Blocking queue returns false from pop().\n";
+                return;
+            }
+        }
+    }
+}
+
+template<template<class> class QueueType>
+FlexiblePool<QueueType>::FlexiblePool(unsigned int num_workers) : num_workers(num_workers), owner(std::this_thread::get_id()) {
+    workers.resize(num_workers);
+    for (unsigned int i = 0; i < num_workers; ++i) {
+        workers[i] = std::thread(&FlexiblePool::do_work, this);
+    }
+}
+
+template<template<class> class QueueType>
+FlexiblePool<QueueType>::FlexiblePool(unsigned int num_workers, QueueType<TaskType> &&tasks) : num_workers(num_workers), tasks(std::move(tasks)), owner(std::this_thread::get_id()) {
+    workers.resize(num_workers);
+    for (unsigned int i = 0; i < num_workers; ++i) {
+        workers[i] = std::thread(&FlexiblePool::do_work, this);
+    }
+}
+
+template<template<class> class QueueType>
+FlexiblePool<QueueType>::~FlexiblePool() {
+    if (!stopped) {
+        stop();
+    }
+}
+
+template<template<class> class QueueType>
+template<class F, class... Args>
+void FlexiblePool<QueueType>::push(F&& f, Args&&... args) {
+    tasks.push([f = std::forward<F>(f), 
+                args_tuple = std::tuple<std::decay_t<Args>...>(std::forward<Args>(args)...)]
+               () mutable noexcept -> ControlSignal {
+        std::apply(std::move(f), std::move(args_tuple));
+        return ControlSignal::OK;
+    });
+}
+
+
+// can only be called by owning thread
+template<template<class> class QueueType>
+void FlexiblePool<QueueType>::stop() {    
+    if (std::this_thread::get_id() != owner) {
+        std::cerr << "[FlexiblePool] [ERROR] Non-owning thread attempted to stop the thread pool.\n";
+        return;
+    }
+    stopped = true;
+    if (!running) {
+        running = true;
+        running.notify_all();
+    }
+    int cur_active_workers;
+    while ((cur_active_workers = num_active_workers) == 0) {
+        num_active_workers.wait(cur_active_workers);
+    }
+    for (size_t i = 0; i < num_workers; ++i) {
+        tasks.push([]() -> ControlSignal {
+            return ControlSignal::STOP; 
+        });
+    }
+    for (size_t i = 0; i < num_workers; ++i) {
+        if (workers[i].joinable()) {
+            workers[i].join();
+        }
+    }
+}
+
+#endif
--- src/ds/pools/simple_pool.h
+++ src/ds/pools/simple_pool.h
@@ -1,130 +1,44 @@
 #ifndef SIMPLE_POOL_H
 #define SIMPLE_POOL_H
 
-#include <vector>
-#include <thread>
-#include <functional>
-#include <iostream>
-#include <mutex>
-#include <atomic>
 #include <chrono>
 
-enum class ControlSignal { OK, RESET, STOP };
-
-// SIMPLEPOOL IS NOT THREAD-SAFE! (ONLY ONE THREAD SUPPOSED TO HAVE ACCESS TO THE THREAD POOL) 
 template<template<class> class QueueType>
 class SimplePool {
-public:
-    using TaskType = std::function<ControlSignal()>;
-    
-    explicit SimplePool(unsigned int num_workers);
-    explicit SimplePool(unsigned int num_workers, QueueType<TaskType> &&tasks);
-    ~SimplePool();
+        unsigned int num_workers;
+        std::vector<std::thread> workers;
+        QueueType<std::function<bool()>> tasks;
+        bool stopped = false;
 
-    void start();
-    
-    template <class F, class... Args>
-    void push(F&& f, Args&&... args);
-    void stop();
-    void reset();
-
-    int get_active_workers() const {
-        return num_active_workers;
-    }
-
-private:
-    unsigned int num_workers;
-    std::vector<std::thread> workers;
-    QueueType<TaskType> tasks;
-    std::thread::id owner;
-
-    bool stopped = false;
-    std::atomic<bool> running{false};
-
-    std::atomic<int> num_active_workers{0};
-
-    void do_work();
-
+        void do_work();
+        // create workers: workers[i] = std::thread(&do_work)
+    public:
+        using TaskType = std::function<bool()>;
+        SimplePool(unsigned int num_workers = 0);
+        ~SimplePool();
+        template <class F, class... Args>
+        void push(F f, Args ... args);
+        void stop();
 };
 
 template<template<class> class QueueType>
-void SimplePool<QueueType>::start() {
-    running = true;
-    running.notify_all();
-}
-
-template<template<class> class QueueType>
-void SimplePool<QueueType>::reset() {
-    if (std::this_thread::get_id() != owner) {
-        std::cerr << "[SimplePool] [ERROR] Non-owning thread attempted to reset the thread pool.\n";
-        return;
-    }
-
-    running = false;
-    running.notify_all();
-    
-    stopped = false;
-
-    for (unsigned int i = 0; i < num_workers; ++i) {
-        tasks.push([]() -> ControlSignal {
-            return ControlSignal::RESET;
-        });
-    }
-    
-    // Wait for all workers to acknowledge reset
-    int cur_active_workers;
-    while ((cur_active_workers = num_active_workers)) {
-        num_active_workers.wait(cur_active_workers);
-    }
-}
-
-template<template<class> class QueueType>
 void SimplePool<QueueType>::do_work() {
-    // Signal that this worker has reset and is now waiting
     while (true) {
-        running.wait(false);
-        num_active_workers.fetch_add(1);
-        num_active_workers.notify_all();
-
-        while (true) {
-            TaskType task;
-            if (tasks.pop(task)) {
-                ControlSignal signal = task();
-                if (signal == ControlSignal::STOP) {
-                    num_active_workers.fetch_sub(1);
-                    num_active_workers.notify_all();
-                    return;
-                }
-                else if (signal == ControlSignal::RESET) {
-                    num_active_workers.fetch_sub(1);
-                    num_active_workers.notify_all();
-                    break;
-                }
-            }
-            else if (!tasks.is_blocking()) { // known at compile-time as is_blocking is constexpr function
-                std::this_thread::yield();
-            }
-            else {
-                std::cerr << "[SimplePool] [FATAL] Blocking queue returns false from pop().\n";
-                return;
-            }
+        // note: using while (true) is fine as SafeUnboundedQueue::pop() blocks until the queue becomes non-empty
+        TaskType task;
+        tasks.pop(task);
+        if (!task()) {
+            return;
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
 template<template<class> class QueueType>
-SimplePool<QueueType>::SimplePool(unsigned int num_workers) : num_workers(num_workers), owner(std::this_thread::get_id()) {
-    workers.reserve(num_workers);
+SimplePool<QueueType>::SimplePool(unsigned int num_workers): num_workers(num_workers) {
+    workers.resize(num_workers);
     for (unsigned int i = 0; i < num_workers; ++i) {
-        workers.emplace_back(&SimplePool::do_work, this);
-    }
-}
-
-template<template<class> class QueueType>
-SimplePool<QueueType>::SimplePool(unsigned int num_workers, QueueType<TaskType> &&tasks) : num_workers(num_workers), tasks(std::move(tasks)), owner(std::this_thread::get_id()) {
-    workers.reserve(num_workers);
-    for (unsigned int i = 0; i < num_workers; ++i) {
-        workers.emplace_back(&SimplePool::do_work, this);
+        workers[i] = std::thread(&SimplePool::do_work, this);
     }
 }
 
@@ -136,37 +50,26 @@ SimplePool<QueueType>::~SimplePool() {
 }
 
 template<template<class> class QueueType>
-template<class F, class... Args>
-void SimplePool<QueueType>::push(F&& f, Args&&... args) {
-    tasks.push([f = std::forward<F>(f), 
-                args_tuple = std::tuple<std::decay_t<Args>...>(std::forward<Args>(args)...)]
-               () mutable noexcept -> ControlSignal {
-        std::apply(std::move(f), std::move(args_tuple));
-        return ControlSignal::OK;
+template <class F, class... Args>
+void SimplePool<QueueType>::push(F f, Args ... args) {
+    // note: cannot capture by reference (possible dangling reference when push returns?)
+    tasks.push([=]() -> bool {
+        f(args...);
+        return true;
     });
 }
 
-
-// can only be called by owning thread
 template<template<class> class QueueType>
-void SimplePool<QueueType>::stop() {    
-    if (std::this_thread::get_id() != owner) {
-        std::cerr << "[SimplePool] [ERROR] Non-owning thread attempted to stop the thread pool.\n";
-        return;
-    }
-    stopped = true;
-    if (!running) {
-        running = true;
-        running.notify_all();
-    }
+void SimplePool<QueueType>::stop() {
     for (unsigned int i = 0; i < num_workers; ++i) {
-        tasks.push([]() -> ControlSignal {
-            return ControlSignal::STOP; 
+        tasks.push([]() -> bool {
+            return false;
         });
     }
-    for (auto& worker : workers) {
-        worker.join();
+    for (unsigned int i = 0; i < num_workers; ++i) {
+        workers[i].join();
     }
+    stopped = true;
 }
 
 #endif
