e26221009be33daadec138dd4fa27498dfa6e277
Duy Nguyen <nhdtxdy@gmail.com>
2025-06-07 07:07:41 +0200

    [ALGO] Re-cycle buckets

--- src/algo/delta_stepping_parallel.h
+++ src/algo/delta_stepping_parallel.h
@@ -17,25 +17,10 @@
 class DeltaSteppingParallel : public ShortestPathSolverBase {
 public:
     const std::string name() const override {
-        return "Naive parallel delta stepping using FlexiblePool";
+        return "Optimized parallel delta stepping";
     }
 
     using Request = Edge;
-    
-    // Verify that std::atomic<Request> is valid
-    static_assert(std::is_trivially_copyable<Request>::value);
-    static_assert(std::is_copy_constructible<Request>::value);
-    static_assert(std::is_move_constructible<Request>::value);
-    static_assert(std::is_copy_assignable<Request>::value);
-    static_assert(std::is_move_assignable<Request>::value);
-    static_assert(std::is_same<Request, typename std::remove_cv<Request>::type>::value);
-
-    static_assert(std::is_trivially_copyable<Request*>::value);
-    static_assert(std::is_copy_constructible<Request*>::value);
-    static_assert(std::is_move_constructible<Request*>::value);
-    static_assert(std::is_copy_assignable<Request*>::value);
-    static_assert(std::is_move_assignable<Request*>::value);
-    static_assert(std::is_same<Request*, typename std::remove_cv<Request*>::type>::value);
 
     DeltaSteppingParallel(double delta, int num_threads): delta(delta), num_threads(num_threads) {}
 
@@ -60,7 +45,6 @@ public:
         
         std::vector<ThreadSafeVector<int>> buckets(1);
         
-        std::atomic<int> max_bucket_size{1};
         std::mutex buckets_resize_mutex;  // Add mutex for resize protection
         buckets[0].push_back(source);
         position_in_bucket[source] = 0;
@@ -73,6 +57,9 @@ public:
 
         std::vector<int> updated_nodes(n);
         std::atomic<size_t> updated_counter{0};
+
+        const int MAX_BUCKET_COUNT = (int)ceil(graph.get_max_edge_weight() / delta) + 5;
+        buckets.resize(MAX_BUCKET_COUNT);
         
         for (int i = 0; i < n; ++i) {
             light_request_map[i].store(std::numeric_limits<double>::infinity());
@@ -86,7 +73,7 @@ public:
             if (dist[v] == INF_MAX) {
                 return -1;
             }
-            return int(dist[v] / delta);
+            return int(dist[v] / delta) % MAX_BUCKET_COUNT;
         };
 
         auto insert_to_corresponding_bucket = [&] (int v) {
@@ -111,15 +98,7 @@ public:
                 size_t write_idx = updated_counter.fetch_add(1);
                 updated_nodes[write_idx] = v;
                 
-                int new_bucket = get_bucket(v);
-                if (new_bucket >= max_bucket_size) {
-                    int desired = new_bucket << 1;
-                    int cur_max_size;
-                    do {
-                        cur_max_size = max_bucket_size.load();
-                    }
-                    while (desired > cur_max_size && !max_bucket_size.compare_exchange_weak(cur_max_size, desired));
-                }
+                // int new_bucket = get_bucket(v);
             }
         };
 
@@ -160,12 +139,21 @@ public:
         // bucket type is either linked list or vector
         FastPool<moodycamel::BlockingConcurrentQueue> pool(num_threads);
 
-        for (int i = 0; i < (int)buckets.size(); ++i) {
-            while (!buckets[i].empty()) {
-                // Loop 1: request generation
+        int generations_without_bucket = 0;
+        for (int generation = 0; ; ++generation, ++generations_without_bucket) {
+            if (generations_without_bucket >= MAX_BUCKET_COUNT) {
+                break;
+            }
+            if (generation >= MAX_BUCKET_COUNT) {
+                generation = 0;
+            }
+            while (!buckets[generation].empty()) {
+                generations_without_bucket = 0;
+
                 {
+                    // Loop 1: request generation
                     pool.start();
-                    ThreadSafeVector<int> &curr_bucket = buckets[i];
+                    ThreadSafeVector<int> &curr_bucket = buckets[generation];
                     int curr_bucket_size = curr_bucket.size();
                     int chunk_size = (curr_bucket_size + num_threads - 1) / num_threads;
                     for (int idx = 0; idx < num_threads; ++idx) {
@@ -174,22 +162,24 @@ public:
                         if (end > curr_bucket_size) {
                             end = curr_bucket_size;
                         }
-                        pool.push([&, start, end] {
-                            for (int idx_u = start; idx_u < end; ++idx_u) {
-                                int u = curr_bucket[idx_u];
-                                if (u >= 0) {
-                                    gen_light_request(u);
+                        if (start < end) {
+                            pool.push([&, start, end] {
+                                for (int idx_u = start; idx_u < end; ++idx_u) {
+                                    int u = curr_bucket[idx_u];
+                                    if (u >= 0) {
+                                        gen_light_request(u);
+                                    }
                                 }
-                            }
-                        });
-                        pool.push([&, start, end] {
-                            for (int idx_u = start; idx_u < end; ++idx_u) {
-                                int u = curr_bucket[idx_u];
-                                if (u >= 0) {
-                                    gen_heavy_request(u);
+                            });
+                            pool.push([&, start, end] {
+                                for (int idx_u = start; idx_u < end; ++idx_u) {
+                                    int u = curr_bucket[idx_u];
+                                    if (u >= 0) {
+                                        gen_heavy_request(u);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        }
                     }
                     pool.reset(); // equivalent to join()
 
@@ -209,12 +199,14 @@ public:
                         if (end > requests_size) {
                             end = requests_size;
                         }
-                        pool.push([&, start, end] {
-                            for (int idx_r = start; idx_r < end; ++idx_r) {
-                                int request_node = light_nodes_requested[idx_r];
-                                relax(request_node, light_request_map);
-                            }
-                        });
+                        if (start < end) {
+                            pool.push([&, start, end] {
+                                for (int idx_r = start; idx_r < end; ++idx_r) {
+                                    int request_node = light_nodes_requested[idx_r];
+                                    relax(request_node, light_request_map);
+                                }
+                            });
+                        }
                     }
                     pool.reset();
 
@@ -225,7 +217,6 @@ public:
                     // Propagate updates to buckets
 
                     pool.start();
-                    buckets.resize(max_bucket_size);
                     int chunk_size = (updated_counter + num_threads - 1) / num_threads;
                     for (int idx = 0; idx < num_threads; ++idx) {
                         int start = idx * chunk_size;
@@ -233,11 +224,13 @@ public:
                         if (end > (int)updated_counter) {
                             end = updated_counter;
                         }
-                        pool.push([&, start, end] {
-                            for (int idx = start; idx < end; ++idx) {
-                                insert_to_corresponding_bucket(updated_nodes[idx]);
-                            }
-                        });
+                        if (start < end) {
+                            pool.push([&, start, end] {
+                                for (int idx = start; idx < end; ++idx) {
+                                    insert_to_corresponding_bucket(updated_nodes[idx]);
+                                }
+                            });
+                        }
                     }
                     pool.reset();
 
@@ -256,12 +249,14 @@ public:
                     if (end > requests_size) {
                         end = requests_size;
                     }
-                    pool.push([&, start, end] {
-                        for (int idx_r = start; idx_r < end; ++idx_r) {
-                            int request_node = heavy_nodes_requested[idx_r];
-                            relax(request_node, heavy_request_map);
-                        }
-                    });
+                    if (start < end) {
+                        pool.push([&, start, end] {
+                            for (int idx_r = start; idx_r < end; ++idx_r) {
+                                int request_node = heavy_nodes_requested[idx_r];
+                                relax(request_node, heavy_request_map);
+                            }
+                        });
+                    }
                 }
                 pool.reset();
 
@@ -272,8 +267,6 @@ public:
                 // propagate updates to buckets
                 pool.start();
                 
-                buckets.resize(max_bucket_size);
-
                 int chunk_size = (updated_counter + num_threads - 1) / num_threads;
                 for (int idx = 0; idx < num_threads; ++idx) {
                     int start = idx * chunk_size;
@@ -281,11 +274,13 @@ public:
                     if (end > (int)updated_counter) {
                         end = updated_counter;
                     }
-                    pool.push([&, start, end] {
-                        for (int idx = start; idx < end; ++idx) {
-                            insert_to_corresponding_bucket(updated_nodes[idx]);
-                        }
-                    });
+                    if (start < end) {
+                        pool.push([&, start, end] {
+                            for (int idx = start; idx < end; ++idx) {
+                                insert_to_corresponding_bucket(updated_nodes[idx]);
+                            }
+                        });
+                    }
                 }
                 pool.reset();
 
